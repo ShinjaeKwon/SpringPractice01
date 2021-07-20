ff9e902c2b853c0654b3e7a9020d34e1fda5cedd
JaeKwonShin <sjk6437@gmail.com>
2021-07-20 17:36:52 +0900

    Repository까지 생성하였다.

--- src/main/java/com/SpringPractice/springboot/web/HelloController.java
+++ src/main/java/com/SpringPractice/springboot/web/HelloController.java
@@ -1,5 +1,7 @@
 package com.SpringPractice.springboot.web;
+import com.SpringPractice.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다.
@@ -12,5 +14,17 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+
+    //@RequestParam
+    //외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+    //여기선 외부에서 name(@RequestParam("name"))이란 이름으로 넘긴 파라미터를 메소드 파라미터 name(String name)에 저장하게 된다.
+
+
 
 }
